025c2c5106597f537e9a156a7ea9c72e4bc9c05e
MorganDawe <mdawe@discoverygarden.ca>
2016-01-19 14:09:43 -0400

    Added collection list details view toggle in theme settings.

--- templates/islandora-basic-collection.tpl.php
+++ templates/islandora-basic-collection.tpl.php
@@ -19,8 +19,18 @@
               <?php endif; ?>
             </dt>
             <dd class="collection-value <?php print isset($associated_object['dc_array']['dc:title']['class']) ? $associated_object['dc_array']['dc:title']['class'] : ''; ?> <?php print $row_field == 0 ? ' first' : ''; ?>">
-              <?php print views_embed_view('collection_item_details_view', 'block', $associated_object['pid']);?>
+              <?php if(theme_get_setting('bceln_collection_items_details')):?>
+                <?php print views_embed_view('collection_item_details_view', 'block', $associated_object['pid']);?>
+              <?php endif;?>
+              <?php if (isset($associated_object['thumb_link']) && !theme_get_setting('bceln_collection_items_details')): ?>
+                <strong><?php print filter_xss($associated_object['title_link']); ?></strong>
+              <?php endif; ?>
             </dd>
+            <?php if (isset($associated_object['dc_array']['dc:description']['value']) && !theme_get_setting('bceln_collection_items_details')): ?>
+              <dd class="collection-value <?php print $associated_object['dc_array']['dc:description']['class']; ?>">
+                <?php print filter_xss($associated_object['dc_array']['dc:description']['value']); ?>
+              </dd>
+            <?php endif; ?>
         </dl>
       </div>
     <?php $row_field++; ?>
